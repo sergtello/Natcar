4d9493c1150e4c05a7c7edb9e731df2b2e9cfc27
sergtello <sergio92tello@gmail.com>
2019-04-05 14:31:18 -0500

    Actulizacion libreria QTR en Desarrollo

--- Desarrollo/Natcar_dev/Natcar_dev.ino
+++ Desarrollo/Natcar_dev/Natcar_dev.ino
@@ -1,9 +1,37 @@
+
 #include <QTRSensors.h>
 
+// This example is designed for use with six analog QTR sensors. These
+// reflectance sensors should be connected to analog pins A0 to A5. The
+// sensors' emitter control pin (CTRL or LEDON) can optionally be connected to
+// digital pin 2, or you can leave it disconnected and remove the call to
+// setEmitterPin().
+//
+// The setup phase of this example calibrates the sensors for ten seconds and
+// turns on the Arduino's LED (usually on pin 13) while calibration is going
+// on. During this phase, you should expose each reflectance sensor to the
+// lightest and darkest readings they will encounter. For example, if you are
+// making a line follower, you should slide the sensors across the line during
+// the calibration phase so that each sensor can get a reading of how dark the
+// line is and how light the ground is. Improper calibration will result in
+// poor readings.
+//
+// The main loop of the example reads the calibrated sensor values and uses
+// them to estimate the position of a line. You can test this by taping a piece
+// of 3/4" black electrical tape to a piece of white paper and sliding the
+// sensor across it. It prints the sensor values to the serial monitor as
+// numbers from 0 (maximum reflectance) to 1000 (minimum reflectance) followed
+// by the estimated location of the line as a number from 0 to 5000. 1000 means
+// the line is directly under sensor 1, 2000 means directly under sensor 2,
+// etc. 0 means the line is directly under sensor 0 or was last seen by sensor
+// 0 before being lost. 5000 means the line is directly under sensor 5 or was
+// last seen by sensor 5 before being lost.
 
-#define NUM_SENSORS             8  // number of sensors used
-#define NUM_SAMPLES_PER_SENSOR  4  // average 4 analog samples per sensor reading
-#define EMITTER_PIN             2  // emitter is controlled by digital pin 2
+QTRSensors qtr;
+
+const uint8_t SensorCount = 8;
+uint16_t sensorValues[SensorCount];
+
 const int pinPWMA = 6;
 const int pinAIN2 = 4;
 const int pinAIN1 = 5;
@@ -11,6 +39,7 @@ const int pinBIN1 = 8;
 const int pinBIN2 = 3;
 const int pinPWMB = 9;
 //const int pinSTBY = 12;
+
 float MD=0;
 float MI=0;
 float dArray = 0;
@@ -24,63 +53,68 @@ float Ia = 0;
 float dt = 0;
 float vcm = 100;
 double t=0;
-// sensors 0 through 5 are connected to analog inputs 0 through 5, respectively
-QTRSensorsAnalog qtra((unsigned char[]) {7, 6, 5, 4, 3, 2, 1, 0},
-  NUM_SENSORS, NUM_SAMPLES_PER_SENSOR, EMITTER_PIN);
-unsigned int sensorValues[NUM_SENSORS];
-
 
 void setup()
 {
+  // configure the sensors
+  qtr.setTypeAnalog();
+  qtr.setSensorPins((const uint8_t[]){A7, A6, A5, A4, A3, A2, A1, A0}, SensorCount);
+  qtr.setEmitterPin(2);
+
   delay(500);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);    // turn on Arduino's LED to indicate we are in calibration mode
-  for (int i = 0; i < 400; i++)  // make the calibration take about 10 seconds
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
+
+  // analogRead() takes about 0.1 ms on an AVR.
+  // 0.1 ms per sensor * 4 samples per sensor read (default) * 6 sensors
+  // * 10 reads per calibrate() call = ~24 ms per calibrate() call.
+  // Call calibrate() 400 times to make calibration take about 10 seconds.
+  for (uint16_t i = 0; i < 400; i++)
   {
-    qtra.calibrate();       // reads all sensors 10 times at 2.5 ms per six sensors (i.e. ~25 ms per call)
+    qtr.calibrate();
   }
-  digitalWrite(13, LOW);     // turn off Arduino's LED to indicate we are through with calibration
+  digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
 
   // print the calibration minimum values measured when emitters were on
   Serial.begin(9600);
-  for (int i = 0; i < NUM_SENSORS; i++)
+  for (uint8_t i = 0; i < SensorCount; i++)
   {
-    Serial.print(qtra.calibratedMinimumOn[i]);
+    Serial.print(qtr.calibrationOn.minimum[i]);
     Serial.print(' ');
   }
   Serial.println();
 
   // print the calibration maximum values measured when emitters were on
-  for (int i = 0; i < NUM_SENSORS; i++)
+  for (uint8_t i = 0; i < SensorCount; i++)
   {
-    Serial.print(qtra.calibratedMaximumOn[i]);
+    Serial.print(qtr.calibrationOn.maximum[i]);
     Serial.print(' ');
   }
   Serial.println();
   Serial.println();
   delay(1000);
 
-  // motores
-  
+  //motores;
 }
-
 
 void loop()
 {
-  // read calibrated sensor values and obtain a measure of the line position from 0 to 5000
-  // To get raw sensor values, call:
-  //  qtra.read(sensorValues); instead of unsigned int position = qtra.readLine(sensorValues);
-  int position = qtra.readLine(sensorValues);
+  // read calibrated sensor values and obtain a measure of the line position
+  // from 0 to 5000 (for a white line, use readLineWhite() instead)
+  uint16_t position = qtr.readLineBlack(sensorValues);
 
-  // print the sensor values as numbers from 0 to 1000, where 0 means maximum reflectance and
-  // 1000 means minimum reflectance, followed by the line position
-  for (unsigned char i = 0; i < NUM_SENSORS; i++)
+  // print the sensor values as numbers from 0 to 1000, where 0 means maximum
+  // reflectance and 1000 means minimum reflectance, followed by the line
+  // position
+  /*for (uint8_t i = 0; i < SensorCount; i++)
   {
-  //  Serial.print(sensorValues[i]);
-  //  Serial.print('\t');
+    Serial.print(sensorValues[i]);
+    Serial.print('\t');
   }
-  //Serial.println(); // uncomment this line if you are using raw values
- // Serial.println(position); // comment this line out if you are using raw values
+  Serial.println(position);
+*/
+  //delay(250);
+
   dArray = (position- 3500)*1 ; 
   dt=micros()/1000000.0-t;
   I = Ia+ki*dArray*dt ;
@@ -117,7 +151,7 @@ void loop()
     Serial.print('\t');
     Serial.print(diff);
     Serial.print('\t');
-Ia = I ;  
+Ia = I ; 
 }
 
 void motores(int izq, int der)
